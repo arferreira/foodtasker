fb9fc41736d285f17555443fcf5afa3ad7b91f15
Antonio Ricardo <antonio.eschola@gmail.com>
2022-11-06 21:37:34 -0400

    feat: implement login and lougout to restaurant

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.urls.conf import include
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', core_views.home, name='home'),
+    path('restaurant/', core_views.restaurant_home, name='restaurant_home'),
     path('restaurant/sign_in/', auth_views.LoginView.as_view(template_name='restaurant/sign_in.html'),                  name='restaurant_sign_in'),
     path('restaurant/sign_out/', auth_views.LogoutView.as_view(next_page='/'), name='restaurant_sign_out'),
 ]
